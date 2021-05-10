dbe3574ebbc241a67b86ccfd6667281cc77be74f
Errett <errett.bentz@gmail.com>
2021-05-09 20:29:34 -0500

    added detail model and view and implemented getById()

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -9,6 +9,7 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
     }
 
     public class InMemoryRestaurantData: IRestaurantData
@@ -20,8 +21,14 @@ namespace OdeToFood.Data
             {
                 new Restaurant{Id = 1, Name = "Errett's Pizza", Location = "Hammond, Louisiana", Cuisine = CuisineType.Italian},
                 new Restaurant{Id = 2, Name = "La Caretta", Location = "Amite, Louisiana", Cuisine = CuisineType.Mexican},
-                new Restaurant{Id = 1, Name = "Taste of India", Location = "Chicago, Illinois", Cuisine = CuisineType.Indian}
+                new Restaurant{Id = 3, Name = "Taste of India", Location = "Chicago, Illinois", Cuisine = CuisineType.Indian},
+                new Restaurant{Id = 4, Name = "J-Lo's Tacos", Location = "South Park, Colorado", Cuisine = CuisineType.Mexican}
             };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
         }
 
         public IEnumerable<Restaurant> GetRestaurantsByName(string name)
