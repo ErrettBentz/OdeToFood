4ddb5c1dd8d10713c9fc8b6df17340d7521471a4
Errett <errett.bentz@gmail.com>
2021-05-10 00:12:01 -0500

    Installed entity framework, created DbContext class

---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext: DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
